ffb277683ce93b1e6bf721c55215109efc9d9de3
Siri <8502281+siri404-gh@users.noreply.github.com>
2024-05-30 03:16:57 +0100

    Update route.ts

--- app/api/route.ts
+++ app/api/route.ts
@@ -3,7 +3,7 @@ export async function POST(request: Request) {
   const rawBodyBuffer = Buffer.from(rawBody, 'utf-8');
   const json = JSON.parse(rawBodyBuffer.toString('utf-8'));
   const { id, payload: { name }, type } = json
-
+  try {
   await fetch('https://discord.com/api/webhooks/1245528235400368248/KWhw4RKvHAWsoka6e3lZvexYzqNfE1fBdxEnLjU-9T7lrSFeHwy5lEbVG7NwUpZPyNgc', {
     method: 'POST',
     "body": "username=a&content=mb&avatar_url=https://avatars.githubusercontent.com/u/14985020?s=200&v=4",
@@ -12,6 +12,6 @@ export async function POST(request: Request) {
     //   content: type,
     //   avatar_url: 'https://avatars.githubusercontent.com/u/14985020?s=200&v=4'
     // })
-  })
+  }) } catch (err) { console.log('===', err) }
   return Response.json({ success: true })
 }
