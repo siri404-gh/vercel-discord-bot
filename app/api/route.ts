15f4746e348180b0947ab4b72d86949fe07261ad
Siri <8502281+siri404-gh@users.noreply.github.com>
2024-05-30 10:41:43 +0100

    Update route.ts

--- app/api/route.ts
+++ app/api/route.ts
@@ -3,17 +3,14 @@ export async function POST(request: Request) {
   const rawBodyBuffer = Buffer.from(rawBody, 'utf-8');
   const json = JSON.parse(rawBodyBuffer.toString('utf-8'));
   const { id, payload: { name }, type } = json
-  console.log(1, name, type)
   try {
-    console.log(2, 'about to post')
     await fetch('https://discord.com/api/webhooks/1245528235400368248/KWhw4RKvHAWsoka6e3lZvexYzqNfE1fBdxEnLjU-9T7lrSFeHwy5lEbVG7NwUpZPyNgc', {
       method: 'POST',
-      // "body": "username=a&content=mb&avatar_url=https://avatars.githubusercontent.com/u/14985020?s=200&v=4",
       headers: {
         'content-type': 'application/json'
       },
       body: JSON.stringify({
-        name: 'Vercel',
+        username: 'Vercel',
         content: type,
         avatar_url: 'https://avatars.githubusercontent.com/u/14985020?s=200&v=4'
       })
