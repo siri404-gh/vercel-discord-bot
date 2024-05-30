61b3a74b0ce976ba2b649e1066a4ae212b747b70
Siri <8502281+siri404-gh@users.noreply.github.com>
2024-05-30 02:46:01 +0100

    Update route.ts

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,4 +1,7 @@
 export async function POST(req: Request) {
-  console.log(req.body)
+  const rawBody = await request.text();
+  const rawBodyBuffer = Buffer.from(rawBody, 'utf-8');
+  const json = JSON.parse(rawBodyBuffer.toString('utf-8'));
+  console.log(json)
   return Response.json({ success: true })
 }
