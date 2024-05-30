bf49bd30cd8e139074d8391826e680ebe96ee529
Siri <8502281+siri404-gh@users.noreply.github.com>
2024-05-30 10:49:35 +0100

    Update route.ts

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,13 +1,14 @@
 import crypto from 'crypto';
 
-const BODY_SIGNATURE = 'P5uBgq3Qvaq3MD69ndDlvBSd'
+const sha1 = crypto.createHash('sha1')
+const INTEGRATION_SECRET = 'P5uBgq3Qvaq3MD69ndDlvBSd'
 const DISCORD_WEBHOOK_URL = 'https://discord.com/api/webhooks/1245528235400368248/KWhw4RKvHAWsoka6e3lZvexYzqNfE1fBdxEnLjU-9T7lrSFeHwy5lEbVG7NwUpZPyNgc'
 
 export async function POST(request: Request) {
   const rawBody = await request.text();
   const rawBodyBuffer = Buffer.from(rawBody, 'utf-8');
   const json = JSON.parse(rawBodyBuffer.toString('utf-8'));
-  const bodySignature = sha1(rawBodyBuffer, BODY_SIGNATURE);
+  const bodySignature = sha1(rawBodyBuffer, INTEGRATION_SECRET);
  
   if (bodySignature !== request.headers.get('x-vercel-signature')) {
     console.log('Invalid bodySignature')
